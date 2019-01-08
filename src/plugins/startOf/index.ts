5e5bf844ec1daed8d0c9e07f94084430c7c64c30
zero <tobewhatwewant@gmail.com>
2019-01-08 18:34:16 +0800

    :sparkles:(plugin/startOf): add startOf plugin

---
+++ src/plugins/startOf/index.ts
@@ -0,0 +1,53 @@
+import { Moment } from '../../index';
+
+declare module '../../index' {
+  export interface Range {
+    start: Moment;
+    end: Moment;
+  }
+
+  export interface Moment {
+    startOfDay(): Moment;
+    startOfWeek(): Moment;
+    startOfMonth(): Moment;
+    startOfYear(): Moment;
+    startOfHour(): Moment;
+    startOfMinute(): Moment;
+    startOfSecond(): Moment;
+  }
+}
+
+export default () => {
+  /* tslint:disable-next-line */
+  return (Moment: Moment) => {
+    const proto = (Moment as any).prototype as Moment;
+
+    proto.startOfDay = function () {
+      return this.startOf('day');
+    };
+
+    proto.startOfWeek = function () {
+      return this.startOf('week');
+    };
+
+    proto.startOfMonth = function () {
+      return this.startOf('month');
+    };
+
+    proto.startOfYear = function () {
+      return this.startOf('year');
+    };
+
+    proto.startOfHour = function () {
+      return this.startOf('hour');
+    };
+
+    proto.startOfMinute = function () {
+      return this.startOf('minute');
+    };
+
+    proto.startOfSecond = function () {
+      return this.startOf('second');
+    };
+  }
+}
